80c644746d573e825d9db92df6056146781333b6
Moises Junior <129017129+MoisesJuh@users.noreply.github.com>
2023-06-05 23:39:25 -0300

    section cadastro e js troca2()

--- style/script.js
+++ style/script.js
@@ -1,7 +1,18 @@
 function troca1(){
   var trocalogin = document.getElementById('login')
+  var trocaregistro = document.getElementById('registro')
 
   if (trocalogin.style.display = 'flex'){
     trocalogin.style.display = 'none'
+    trocaregistro.style.display = 'flex'
+  }
+}
+function troca2(){
+  var trocalogin = document.getElementById('login')
+  var trocaregistro = document.getElementById('registro')
+
+  if (trocaregistro.style.display = 'flex'){
+   trocalogin.style.display = 'flex'
+   trocaregistro.style.display = 'none' 
   }
 }
